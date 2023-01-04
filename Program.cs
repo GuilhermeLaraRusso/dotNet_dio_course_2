00484ed782a3aced40578a88335fc77802b115a2
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2023-01-04 10:59:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using ExemploExplorando.Models;
+
+
+Pessoa p1 = new Pessoa("Guilherme", "Russo");
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "João";
+p2.Sobrenome = "Silva";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+int quantidadeDeAlunos = cursoDeIngles.QuantidadeAlunos();
+Console.WriteLine($"Quantidade de alunos: {quantidadeDeAlunos}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Guilherme";
+// p1.Sobrenome = "Russo";
+// p1.Idade = 34;
+// p1.Apresentar();
